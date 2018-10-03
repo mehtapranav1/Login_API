ee8bfe552fda22ce345029eaebda46c18cc90c0e
mehtapranav1 <96.pranavmehta@gmail.com>
2018-10-03 11:24:21 +0530

    Add files via upload

---
+++ login_api/objects/user.php
@@ -0,0 +1,91 @@
+<?php
+class User{
+ 
+    // database connection and table name
+    private $conn;
+    private $table_name = "users";
+ 
+    // object properties
+
+    public $userid;
+    public $username;
+    public $password;
+    public $email;
+    public $referencedby;
+    public $referalcode;
+ 
+    // constructor with $db as database connection
+    public function __construct($db){
+        $this->conn = $db;
+    }
+    // signup user
+    function signup(){
+    
+        if($this->isAlreadyExist()){
+            return false;
+        }
+        // query to insert record
+        $query = "INSERT INTO
+                    " . $this->table_name . "
+                SET
+                    username=:username, email=:email, password=:password, referencedby=:referencedby,   referalcode=:referalcode";
+                   
+        // prepare query
+        $stmt = $this->conn->prepare($query);
+    
+        // sanitize
+        $this->username=htmlspecialchars(strip_tags($this->username));
+        $this->email=htmlspecialchars(strip_tags($this->email));
+        $this->password=htmlspecialchars(strip_tags($this->password));
+        $this->referencedby=htmlspecialchars(strip_tags($this->referencedby));
+        $this->referalcode=htmlspecialchars(strip_tags($this->referalcode));
+
+        // bind values
+        $stmt->bindParam(":username", $this->username);
+        $stmt->bindParam(":email", $this->email);
+        $stmt->bindParam(":password", $this->password);
+        $stmt->bindParam(":referencedby", $this->referencedby);
+        $stmt->bindParam(":referalcode", $this->referalcode);
+    
+        // execute query
+        if($stmt->execute()){
+            $this->id = $this->conn->lastInsertId();
+            return true;
+        }
+    
+        return false;
+        
+    }
+    // login user
+    function login(){
+        // select all query
+        $query = "SELECT
+                    `userid`, `username`, `password`
+                    FROM
+                    " . $this->table_name . " 
+                WHERE
+                    username='".$this->username."' AND password='".$this->password."'";
+        // prepare query statement
+        $stmt = $this->conn->prepare($query);
+        // execute query
+        $stmt->execute();
+        return $stmt;
+    }
+    function isAlreadyExist(){
+        $query = "SELECT *
+            FROM
+                " . $this->table_name . " 
+            WHERE
+                username='".$this->username."'";
+        // prepare query statement
+        $stmt = $this->conn->prepare($query);
+        // execute query
+        $stmt->execute();
+        if($stmt->rowCount() > 0){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
